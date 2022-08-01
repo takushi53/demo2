b6981217f359a7e9431124798bd5300b56a392f8
takuya morishita <102013434+takushi53@users.noreply.github.com>
2022-08-01 18:14:26 +0900

    認証機能の実装完了

---
+++ src/main/java/com/example/demo2/service/AccountUserDetails.java
@@ -0,0 +1,61 @@
+package com.example.demo2.service;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.demo2.data.entity.Users;
+
+public class AccountUserDetails implements UserDetails {
+	private Users user;
+	
+	public AccountUserDetails(Users user) {
+		this.user = user;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority>  getAuthorities() {
+		return AuthorityUtils.createAuthorityList("ROLE_" + user.getRoleName());
+	}
+	
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+	
+	@Override
+	public String getUsername() {
+		return user.getUserName();
+	}
+	
+	@Override	
+	public boolean isAccountNonExpired() {	
+		return true;	
+	}
+	
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
+	public Users getUser() {
+		return user;
+	}
+	
+	public String getName() {
+		return user.getName();
+	}
+	
+}
